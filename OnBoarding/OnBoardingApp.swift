6667dfb4963f2541f8f3bc96b943be6bdf2558f6
Johnny <29jmeyer@gmail.com>
2025-12-01 20:13:47 -0500

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by john meyer on 2025-12-01.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
